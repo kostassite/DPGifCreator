a97292b83c9999fded4ebe62d7333e401c0c44a2
Kostas Antonopoulos <kostas@devpro.gr>
2015-04-02 02:47:13 +0300

    Initial Commit

---
+++ DPGifCreator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DPGifCreator
+//
+//  Created by Kostas on 2/4/15.
+//  Copyright (c) 2015 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DPGifCreator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DPGifCreator
+//
+//  Created by Kostas on 2/4/15.
+//  Copyright (c) 2015 Kostas Antonopoulos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
